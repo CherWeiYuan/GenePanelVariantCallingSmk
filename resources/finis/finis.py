3dde76c9d6bdb0daba470bb8d2e6af2a22c43e49
WY <47816698+CherWeiYuan@users.noreply.github.com>
2023-03-28 10:49:13 +0800

    Add features to finis
    Use dask to replace pandas to reduce memory requirements. Also, add options to not output full dataframe and remove the need to parse SpliceAI column.

--- resources/finis/finis.py
+++ resources/finis/finis.py
@@ -11,8 +11,8 @@ with gNOMAD v3.1.2 gene-specific data, filters the TSV based on user-supplied
 tags and cleans & plot the final output
 """
 
-from argparse import ArgumentParser
-import pandas as pd
+from argparse import ArgumentParser, BooleanOptionalAction
+import dask.dataframe as dd
 from numpy import nan
 import sys
 
@@ -44,16 +44,25 @@ def parse_args():
     parser.add_argument("--out_full",
                         type = str,
                         required = True,
-                        help = "Output path and file prefix, e.g. /mnt/e/out")
+                        help = "Output path and file prefix, e.g. /mnt/e/out " +\
+                               "Set to '0' if full output is not required")
     parser.add_argument("--out_simple",
                         type = str,
                         required = True,
                         help = "Output path and file prefix, e.g. /mnt/e/out " +\
                                "Set to '0' if simple output is not required")
+    parser.add_argument("--skip_spliceai",
+                        action = BooleanOptionalAction,
+                        help = "Add flag to not process SpliceAI column")
     parser.add_argument("--column_name",
                         type = str,
                         default = "SpliceAI_SpliceAI",
                         help = "Column name to process")
+    parser.add_argument("--chunksize",
+                        type = int,
+                        default = 1000000,
+                        help = "Rows to write at a time")
+
     return parser.parse_args()
 
 def removeComments(inputFileName, outputFileName):
@@ -101,7 +110,7 @@ def parse_splice_predictions(vcfdf, column_name):
         row_high_scores = []
         gene_name = vcfdf.loc[index, "SYMBOL"]
         try:
-            if pd.isnull(vcfdf.loc[index, column_name]):
+            if dd.isnull(vcfdf.loc[index, column_name]):
                 continue
             variant_records = vcfdf.loc[index, column_name].split(",")
             for record_set in variant_records:
@@ -135,35 +144,56 @@ def main():
     vcf_tsv = args.vcf_tsv
     out_full = args.out_full
     out_simple = args.out_simple
+    skip_spliceai = args.skip_spliceai
     column_name = args.column_name
+    chunksize = args.chunksize
     
-    # Load dataframes and merge
+    # Load dataframes and remove lines starting with double hex
     removeComments(vcf_tsv, vcf_tsv + ".noHex.tsv")
     try:
-        vcf_df = pd.read_csv(vcf_tsv + ".noHex.tsv", sep = "\t", header = 1)
-        vcf_df = parse_splice_predictions(vcf_df, column_name)
+        vcf_df = dd.read_csv(vcf_tsv + ".noHex.tsv", sep = "\t", header = 1)
+        if skip_spliceai:
+            pass
+        else:
+            vcf_df = parse_splice_predictions(vcf_df, column_name)
     except KeyError:
-        vcf_df = pd.read_csv(vcf_tsv + ".noHex.tsv", sep = "\t")
-        vcf_df = parse_splice_predictions(vcf_df, column_name)     
+        vcf_df = dd.read_csv(vcf_tsv + ".noHex.tsv", sep = "\t")
+        if skip_spliceai:
+            pass
+        else:
+            vcf_df = parse_splice_predictions(vcf_df, column_name)     
            
     # Output full csv
-    vcf_df.to_csv(out_full, index = False)
+    if out_full != "0":
+        vcf_df.to_csv(out_full, index = False)
 
     # Output simplified csv
     if out_simple != "0":
-        vcf_df = vcf_df[[
-            "HGVSg",
-            "HGVSc",
-            "HGVSp",
-            "SYMBOL",
-            "ZYG",
-            "Existing_variation",
-            "Consequence",
-            "gnomADg_AF",
-            "gnomADg_EAS_AF",
-            "ClinVar_CLNSIG",
-            "SpliceAI_SpliceAI_highest_score"]]
-
+        if skip_spliceai:
+            vcf_df = vcf_df[[
+                "HGVSg",
+                "HGVSc",
+                "HGVSp",
+                "SYMBOL",
+                "ZYG",
+                "Existing_variation",
+                "Consequence",
+                "gnomADg_AF",
+                "gnomADg_EAS_AF",
+                "ClinVar_CLNSIG"]]
+        else:
+            vcf_df = vcf_df[[
+                "HGVSg",
+                "HGVSc",
+                "HGVSp",
+                "SYMBOL",
+                "ZYG",
+                "Existing_variation",
+                "Consequence",
+                "gnomADg_AF",
+                "gnomADg_EAS_AF",
+                "ClinVar_CLNSIG",
+                "SpliceAI_SpliceAI_highest_score"]]
     vcf_df.to_csv(out_simple, index = False)
     
 # Run main() if this script is called from command-line    
